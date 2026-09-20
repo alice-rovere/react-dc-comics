20187a15a45bdc1adb3ed917700cf3045458fee7
alice-rovere <arovere1@gmail.com>
2026-09-20 17:44:14 +0200

    abbozzata hero section

--- src/layout/HeroSection.jsx
+++ src/layout/HeroSection.jsx
@@ -1,3 +1,12 @@
+import hero from "../assets/img/jumbotron.jpg";
 export default function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <div>
+      <img
+        src={hero}
+        alt="immagine della dc comics"
+        className="w-full h-96 object-cover"
+      />
+    </div>
+  );
 }
